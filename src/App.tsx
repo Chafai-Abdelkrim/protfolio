eb044880ded4b13b49a4c8f6e599807f053b0743
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-21 08:28:25 +0000

    added background to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-
-
+import { About, Home, Projects } from './components';
+import background from './assets/stacked-waves-haikei.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${background})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+    >
       <Home />
       <About />
       <Projects />
     </div>
-  )
+  );
 }
 
 export default App;
