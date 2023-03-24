5a35709cf5cfd076112ff589f05b62a948540115
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-24 02:08:16 +0000

    started work on the projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,20 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { useSmoothScroll } from '../hooks';
+
 const Projects = () => {
-  return <div></div>;
+  const container = useRef(null);
+  const spring = useSmoothScroll(container);
+
+  return (
+    <motion.div
+      ref={container}
+      id="projects"
+      className="grid h-[100svh] grid-cols-1 grid-rows-[2fr,min-content,1fr] items-center justify-items-center gap-4 md:grid-cols-2 md:grid-rows-[2fr,1fr]"
+    >
+      <motion.div></motion.div>
+    </motion.div>
+  );
 };
 
 export default Projects;
