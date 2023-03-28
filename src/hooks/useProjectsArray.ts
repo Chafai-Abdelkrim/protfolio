ba0e6d30a62608b38c2ffc474bda836f0b5f6228
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-28 00:31:52 +0000

    added useProjectsArray hook

--- src/hooks/useProjectsArray.ts
+++ src/hooks/useProjectsArray.ts
@@ -1,5 +1,23 @@
 import { useState } from 'react';
+import { projectsArray } from '../data';
 
 const useProjectsArray = () => {
   const [page, setPage] = useState(0);
+  const currentProject = projectsArray[page];
+
+  const pageNext = () => {
+    setPage((prev) => (prev + 1) % projectsArray.length);
+  };
+
+  const pagePrev = () => {
+    setPage((prev) => (prev - 1 < 0 ? projectsArray.length - 1 : prev - 1));
+  };
+
+  return {
+    currentProject,
+    pageNext,
+    pagePrev,
+  };
 };
+
+export default useProjectsArray;
