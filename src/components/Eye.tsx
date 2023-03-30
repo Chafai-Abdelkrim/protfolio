4482635d401b8f896afb3ffbb44cb1878e9f4f46
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-30 12:30:04 +0000

    added watching-you and eyes

---
+++ src/components/Eye.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import WatchingYou from 'react-watching-you';
+
+const StyledEye = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+  width: 37.8px;
+  border-radius: 50%;
+  box-shadow: inset 0px 0px 6px #000;
+  background: #fff;
+`;
+const EyeBall = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 24px;
+  width: 24px;
+  border-radius: 50%;
+  background: rgb(112, 56, 0);
+  background: linear-gradient(
+    135deg,
+    rgba(112, 56, 0, 1) 40%,
+    rgba(242, 201, 0, 1) 100%
+  );
+`;
+const EyeBall1 = styled.div`
+  position: relative;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  background: #2f1100;
+`;
+const EyeBall2 = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 8px;
+  width: 8px;
+  border-radius: 50%;
+  background: #fff;
+`;
+const power = { x: 15, y: 26 };
+
+const Eye = () => {
+  return (
+    <StyledEye>
+      <WatchingYou power={power} rotatable={false}>
+        <EyeBall>
+          <EyeBall1>
+            <EyeBall2 />
+          </EyeBall1>
+        </EyeBall>
+      </WatchingYou>
+    </StyledEye>
+  );
+};
+
+export default Eye;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,7 @@ import {
   MdOutlineMailOutline,
 } from 'react-icons/md';
 import { motion } from 'framer-motion';
+import Eye from './Eye';
 
 const Home = () => {
   const container = useRef<HTMLDivElement | null>(null);
@@ -13,12 +14,16 @@ const Home = () => {
 
   return (
     <div id="Home" ref={container} className="grid h-[100svh] grid-rows-[1fr]">
-      <header className="fixed z-50 flex w-full justify-end p-2">
+      <header className="fixed z-50 flex w-full justify-between p-2">
+        <div className="flex gap-4">
+          <Eye />
+          <Eye />
+        </div>
         <a
-          className="fold relative text-base text-[#58545B]"
+          className="fold-r relative text-base text-[#58545B]"
           href="mailto:chafai.abdelkrim@gmail.com"
         >
-          <MdOutlineMailOutline size={'2em'} />
+          <MdOutlineMailOutline size={'2.5em'} />
         </a>
       </header>
       <div className="flex flex-1 flex-col items-center justify-center text-center leading-none">
