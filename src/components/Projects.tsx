c574148837a036511b93386ca639c8be346ebbcb
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-31 11:00:51 +0000

    tweaked aspect

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -87,7 +87,7 @@ const Projects = () => {
             <div className="bg-orange-500"></div>
             <div className="bg-green-500"></div>
           </div>
-          <div className="aspect-[16/9] w-full">
+          <div className="aspect-[16/8.6] w-full">
             <AnimatePresence>
               <motion.img
                 initial={{ opacity: 0 }}
